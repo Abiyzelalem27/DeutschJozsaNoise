4155f21fe9abf651aa537c941d9d7b8e265859f8
Abiyzelalem27 <abiyzelalem8@gmail.com>
2026-08-04 12:11:03 +0000

    modified pyproject.toml

---
+++ build/lib/quantum_algorithms/__init__.py
@@ -0,0 +1,11 @@
+
+
+from . import operators, black_box, deutsch_jozsa, depolarizing, plotting
+
+__all__ = [
+    "operators",
+    "black_box",
+    "deutsch_jozsa",
+    "depolarizing",
+    "plotting"
+] 
